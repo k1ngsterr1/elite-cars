0363e1f6acc8a04aaeca0ca380deb828c7b6dde2
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-10-25 22:44:57 +0600

    Version 1.2.2

--- src/components/CarForm/BookingForm.tsx
+++ src/components/CarForm/BookingForm.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import Select from "react-select";
+import DatePicker from "react-datepicker";
+import TimePicker from "react-time-picker";
+import "react-datepicker/dist/react-datepicker.css"; // Import the CSS
+import "react-time-picker/dist/TimePicker.css";
+import "react-clock/dist/Clock.css";
 
 import "./styles/car-form.css";
 
@@ -20,6 +25,10 @@ const luggageOptions = [
 ];
 
 const BookingForm: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [selectedTime, setSelectedTime] = useState<string>("10:00");
+
+  const [currentStep, setCurrentStep] = useState(1);
   const [passengers, setPassengers] = useState(1);
   const [carType, setCarType] = useState(null);
   const [serviceType, setServiceType] = useState(null);
@@ -29,11 +38,17 @@ const BookingForm: React.FC = () => {
     setPassengers(1);
   }
 
-  const isStep2Visible = carType && serviceType && luggage && passengers;
-
+  const handleNextClick = () => {
+    if (carType && serviceType && luggage && passengers) {
+      setCurrentStep(2);
+    } else {
+      // You can add some alert or notification to inform user to fill all fields
+      alert("Please fill out all fields before proceeding!");
+    }
+  };
   return (
     <div className="form-container">
-      {!isStep2Visible ? (
+      {currentStep === 1 ? (
         <>
           <h3 className="form-heading">Step 1</h3>
           <div className="form-group mt32">
@@ -44,7 +59,6 @@ const BookingForm: React.FC = () => {
               onChange={(option: any) => setCarType(option?.value)}
             />
           </div>
-
           <div className="form-group mt32">
             <label className="label">Select Service Type</label>
             <Select
@@ -53,7 +67,6 @@ const BookingForm: React.FC = () => {
               onChange={(option: any) => setServiceType(option?.value)}
             />
           </div>
-
           <div className="form-group mt32">
             <label className="label">Luggage</label>
             <Select
@@ -62,7 +75,6 @@ const BookingForm: React.FC = () => {
               onChange={(option: any) => setLuggage(option?.value)}
             />
           </div>
-
           <div className="form-group mt32">
             <label className="label">Passengers</label>
             <div className="counter">
@@ -81,11 +93,57 @@ const BookingForm: React.FC = () => {
               </button>
             </div>
           </div>
-
-          <button className="submit-button">Next</button>
+          <button className="submit-button" onClick={handleNextClick}>
+            Next
+          </button>
         </>
       ) : (
-        <h3>aaa</h3>
+        <>
+          {" "}
+          <h3 className="form-heading">Step 2</h3>
+          <div className="form-group mt32">
+            <label className="label">Pickup Location</label>
+            <input
+              type="text"
+              placeholder="Enter Location"
+              className="b-form"
+            />
+          </div>
+          <div className="form-group mt32">
+            <label className="label">Dropoff Location</label>
+            <input
+              type="text"
+              placeholder="Enter Location"
+              className="b-form"
+            />
+          </div>
+          <div className="form-group mt32">
+            <label className="label">Pick-up Date</label>
+            <DatePicker
+              selected={selectedDate}
+              onChange={(date: Date) => setSelectedDate(date)}
+              dateFormat="MMMM d, yyyy"
+              className="b-form"
+              placeholderText="Select Date"
+            />
+          </div>
+          <div className="form-group mt32">
+            <label className="label">Pick-up Time</label>
+            <TimePicker
+              value={selectedTime}
+              onChange={(value) => {
+                if (value) {
+                  setSelectedTime(value);
+                }
+              }}
+              className="b-form"
+              disableClock={true}
+            />
+          </div>
+          <button className="submit-button" onClick={handleNextClick}>
+            Next
+          </button>
+        </>
       )}
     </div>
   );
