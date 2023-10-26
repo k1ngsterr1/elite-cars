dd1b9d475e2a50c9980e1807b62490953389cdb9
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-10-26 17:18:29 +0600

    Version 1.2.8

--- src/screens/RatesScreen/RatesScreen.tsx
+++ src/screens/RatesScreen/RatesScreen.tsx
@@ -8,8 +8,8 @@ const rate04 = require("../../assets/rates_04.webp");
 
 const RatesScreen = () => {
   return (
-    <section className="screen">
-      <main className="content">
+    <section className="screen mt128">
+      <div className="content">
         <h2 className="heading-s">
           Our <span className="orange">rates</span>
         </h2>
@@ -45,7 +45,46 @@ const RatesScreen = () => {
           carHourPrice="$136.00"
           carMilePrice="$3.50"
         />
-      </main>
+      </div>
+      <div className="content-pc column">
+        <h2 className="heading-s">
+          Our <span className="orange">rates</span>
+        </h2>
+        <div className="rates-list">
+          <CarRate
+            marginTop="mt64"
+            carImage={rate01}
+            carCategorie="Premium Sedan"
+            carName="Cadillac CT6 Luxury"
+            carHourPrice="$136.00"
+            carMilePrice="$3.00"
+          />
+          <CarRate
+            marginTop="mt32"
+            carImage={rate02}
+            carCategorie="Premium Sedan"
+            carName="Mercedes-Benz S-Class S 580"
+            carHourPrice="$188.00"
+            carMilePrice="$3.50"
+          />
+          <CarRate
+            marginTop="mt32"
+            carImage={rate03}
+            carCategorie="Premium Sedan"
+            carName="BMW 7-series 740i Xdrive"
+            carHourPrice="$188.00"
+            carMilePrice="$3.50"
+          />
+          <CarRate
+            marginTop="mt32"
+            carImage={rate04}
+            carCategorie="Luxury SUV"
+            carName="2023 Cadillac Escalade ESV Sport"
+            carHourPrice="$136.00"
+            carMilePrice="$3.50"
+          />
+        </div>
+      </div>
     </section>
   );
 };
